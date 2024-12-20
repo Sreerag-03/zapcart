60d0c5e6e10fb188d909131d0233472d6992cb2a
Sreerag-03 <nairsreerag2@gmail.com>
2024-12-20 21:15:33 +0530

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zapcart/core/configs/theme/app_theme.dart';
+import 'package:zapcart/firebase_options.dart';
 import 'package:zapcart/presentation/splash/bloc/splash_cubit.dart';
 import 'package:zapcart/presentation/splash/pages/splash.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
